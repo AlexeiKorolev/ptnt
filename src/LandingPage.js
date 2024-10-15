6ccc7ebdb43245cd0b59ff52b76f7da4b0b76013
ScopeStudios <alexeithepro@gmail.com>
2024-10-14 22:43:57 -0400

    logo

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -4,7 +4,7 @@ import './LandingPage.css';
 function LandingPage() {
   return (
     <div className="landing-page">
-        <img src="/path-to-your-logo.png" alt="Logo" className="logo" />
+        <img src="A:\Projects\PatentFrontend\patent_revamp\src\logo.jpg" alt="Logo" className="logo" />
         <h1 className="title">Mako Intelligence</h1>
         <button className="get-started-btn">Get Started</button>
     </div>
